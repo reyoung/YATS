d6adb0294c78f6a524be34d7a6ef50d95008bbd3
reyoung <reyoung@126.com>
2011-05-30 09:35:00 +0800

    Add Teacher Draft Page and Left tag

--- app/controllers/Teacher.java
+++ app/controllers/Teacher.java
@@ -1,28 +1,43 @@
 /*
  * This file is written by reyoung, reyoung@126.com.
  */
-
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
 import models.User;
 import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.With;
+
 /**
  *
  * @author Reyoung
  */
 @Check("teacher")
 @With(Secure.class)
-public class Teacher extends Controller{
+public class Teacher extends Controller {
+
     @Before
     static void setConnectedUser() {
-        if(Security.isConnected()) {
+        if (Security.isConnected()) {
             User user = User.find("byName", Security.connected()).first();
             renderArgs.put("user", user);
         }
+        List<HeaderItem> headeritem = new ArrayList<HeaderItem>();
+        headeritem.add(new HeaderItem("/teacher/draft", "Draft"));
+        headeritem.add(new HeaderItem("/teacher/published","Published"));
+        headeritem.add(new HeaderItem("/about", "About"));
+        renderArgs.put("headeritem", headeritem);
+        renderArgs.put("headerimg", "/public/images/logo.bmp");
     }
-    public static void index(){
+    /**
+     * 草稿页面
+     */
+    public static void draft(){
         render();
+    }
+    public static void index() {
+        draft();                //! 默认跳转到草稿页面
     }
 }
